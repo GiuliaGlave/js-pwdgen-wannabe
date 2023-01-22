1d8341caadbb5757fa5883bae506b3913fef32bb
GiuliaGlave <giulia.glave@gmail.com>
2023-01-22 22:23:50 +0100

    add js

---
+++ script.js
@@ -0,0 +1,5 @@
+let user_name = prompt('Il tuo nome');
+let user_surname = prompt('Il tuo cognome');
+let user_color = prompt('Il tuo colore preferito');
+
+document.getElementById('password').innerHTML = ` ${user_name}  ${user_surname}  ${user_color}  23 `;
